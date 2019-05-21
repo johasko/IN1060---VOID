e581b379de6d941e641a8e16fed7c6db17ad7c87
Johannes Skøien <johannes_skoien@hotmail.com>
2019-05-22 01:43:47 +0200

    Kodefil med fungerende oppsett med seriekobling

---
+++ Kode_slave/Kode_slave.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <RFID.h>
+
+#define SS_PIN        10
+#define RST_PIN       9
+
+RFID rfid(SS_PIN, RST_PIN);
+
+const int power = 7;
+const int led = 8; 
+int serNum[5];
+int kategori;
+
+
+int cards[][5] = {{138,84,37,131,120}};
+
+#define SLAVE_ADDR    9
+#define ANSWER_SIZE   1
+byte svar[ANSWER_SIZE];
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.init();
+  pinMode(power, OUTPUT);
+  digitalWrite(power, LOW);
+  Wire.begin(SLAVE_ADDR);
+  Wire.onRequest(requestEvent);
+}
+
+void requestEvent() {
+  int kort; // Kategorien bestemmes ut i fra rfid-kortets forste rekke med ID.
+    
+    if(rfid.isCard()){
+    
+        if(rfid.readCardSerial()){
+            kort = rfid.serNum[1];
+            
+        }
+
+        if(kort == 197){
+            kategori = 1;
+        }else if(kort == 84){
+            kategori = 2;
+        }
+
+        Wire.write(kategori);
+
+    }
+    
+    
+    
+    rfid.halt();
+  
+}
+
+void loop() {
+  delay(50);
+}
--- Kodefil_v2/Kodefil_v2.ino
+++ Kodefil_v2/Kodefil_v2.ino
@@ -1,26 +1,25 @@
-/* Inputs:
- * Munn = 7
- * Øyne = 8;
- */
-
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
  #include <avr/power.h>               // Required for 16 MHz Adafruit Trinket
 #endif
 
-#include <SPI.h>
-#include <MFRC522.h>
+#include <Wire.h>                     // Library for sammenkobling av arduinoer
 
 #define MUNN           7
 #define OYNE           8
 
-#define RST_PIN        6
-#define SS_PIN         5
+//#define RST_PIN        6
+//#define SS_PIN         5
+
+#define SLAVE_ADDR     9              //Definerer nummer for slave-Arduino
+#define ANSWER_SIZE    1              //Størrelse på forventet svar fra slave
 
 #define PIXELMUNN      15             //Antall LEDs i munn
 #define PIXELOYE       12             //Antall LEDs i oynene
 
-// unsigned long forrigeTid = 0;
+unsigned long forrigeTid = 0;
+
+int rfidKort[] = {1, 2, 3};
 
 unsigned long tidNaa = 0;
 int forsinkelseSnurr = 125;
@@ -38,15 +37,13 @@ int svarS2 = 0;
 
 Adafruit_NeoPixel munn(PIXELMUNN, MUNN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel oyne(PIXELOYE, OYNE, NEO_GRB + NEO_KHZ800);
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+//MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
 
   Serial.begin(9600);
-  SPI.begin();                       // Initierer SPI bus for RFID-scanner
-  mfrc522.PCD_Init();                // Initialiserer MFRC522-scanneren
-
   delay(3000);                       //Sikkerhetsdelay ved oppstart for å hindre kortslutting
+  Wire.begin();
   munn.begin();                      // Initialiserer LED-strip
   oyne.begin();
   munn.setBrightness(40);            //Justerer lysstyrke på LEDs
@@ -62,10 +59,32 @@ void loop() {
   munn.clear();                     // Slår av alle pixler i munn
   oyne.clear();                     // Slår av alle pixler i øyne
 
+  //Wire.beginTransmission(SLAVE_ADDR);
+  //Wire.write(0);
+  //Wire.endTransmission();
+  int kort;
+  kort = 0;
+  //Serial.println("Start");
+  delay(1000);
+  while (kort == 0) {
+    Wire.requestFrom(SLAVE_ADDR, 1);
+  
+    while (Wire.available()) {
+      //Serial.println("Leser kort");
+      kort = Wire.read();
+     }
+     //Serial.println("Lest kort");
+  }
 
+  
+  Serial.print("KategoriID: ");
+  Serial.println(kort);
+  delay(2000);
+  //nedtelling();
 
-  nedtelling();
+}
 
+void velgKat(int kort) {
 }
 
 void munnPaa() {                    //Setter alle LEDs i munnen til å være på med hvitt lys
@@ -112,10 +131,11 @@ void oyeSnurr() {
   for (int i = 0; i < PIXELOYE; i++) {
     oyne.setPixelColor(i, oyne.Color(255, 255, 255));
     oyne.show();
-    tidNaa = millis();
+    forsinkelse(125);
+    /*tidNaa = millis();
     while ((millis() - tidNaa) >= forsinkelseSnurr) {
         sjekkTrykk();
-    }
+    }*/
   }
 
   for (int i = 0; i < PIXELOYE; i++) {
